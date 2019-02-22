e62b1ebd084bb8c28d40c67db24e3451c81b089b
bonnw <pwonghansa@gmail.com>
2019-02-22 15:34:15 -0600

    initialized project with intelliJ

---
+++ src/main/java/com/burahan/resttodo/ResttodoApplication.java
@@ -0,0 +1,15 @@
+package com.burahan.resttodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResttodoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ResttodoApplication.class, args);
+    }
+
+}
